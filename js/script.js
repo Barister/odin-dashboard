72ebd978edb3a5dc68f9f5a287ab566435d7d991
Evgeny Liskevich <59793749+Barister@users.noreply.github.com>
2023-07-31 15:41:23 +0600

    Aside minimaze ready

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 //burger activation click
 
 const burger = document.querySelector('.burger-menu');
+const asidePanel = document.querySelector('.aside');
+const logoMobile = document.querySelector('.aside__logoMobile');
+const body = document.querySelector('body');
 
 burger.addEventListener('click', () => {
    burger.classList.toggle('active');
+   asidePanel.classList.toggle('active');
+   logoMobile.classList.toggle('active');
+   body.classList.toggle('active');
 });
-console.log(burger);
+console.log(asidePanel);
