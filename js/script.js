b3a9fbc2dc506809dc05c1267adc193b750a3e98
Evgeny Liskevich <59793749+Barister@users.noreply.github.com>
2023-08-04 23:41:19 +0600

    Total revenue card ready

--- js/script.js
+++ js/script.js
@@ -35,6 +35,79 @@ function formatDate(date) {
 
 const today = new Date();
 const formattedDate = formatDate(today);
-console.log("Today is", formattedDate);
+//console.log("Today is", formattedDate);
 
 todayBlock.textContent = formattedDate;
+
+
+//total-revenue__graph
+
+document.addEventListener("DOMContentLoaded", function () {
+   // Получить элемент canvas для рисования графика
+   var ctx = document.getElementById("two-axis-chart").getContext("2d");
+
+   // Данные для оси X
+   var labels = ["0", "4", "8", "12", "16", "20", "24", "28"];
+
+   // Данные для "Rents collected"
+   var rentsData = [120, 150, 180, 190, 180, 175, 180, 160];
+
+   // Данные для "Expenses"
+   var expensesData = [80, 100, 90, 97, 63, 100, 77, 100];
+
+   // Создать двухосевую диаграмму
+   var twoAxisChart = new Chart(ctx, {
+      type: "line",
+      data: {
+         labels: labels,
+         datasets: [
+            {
+               label: "Rents collected",
+               data: rentsData,
+               borderColor: "#4d2cee",
+               //backgroundColor: "rgba(0, 123, 255, 0.2)",
+               fill: false,
+            },
+            {
+               label: "Expenses",
+               data: expensesData,
+               borderColor: "#31c240",
+               //backgroundColor: "rgba(220, 53, 69, 0.2)",
+               fill: false,
+            },
+         ],
+      },
+      options: {
+         maintainAspectRatio: false,
+         scales: {
+            x:
+            {
+               ticks: {
+                  beginAtZero: true,
+                  stepSize: 4,
+               },
+            },
+
+            y:
+            {
+               ticks: {
+                  beginAtZero: true,
+                  max: 200,
+                  stepSize: 100,
+                  callback: function (value, index, ticks) {
+                     return '$' + value;
+                  },
+               },
+            },
+
+         },
+         plugins: {
+            legend: false,
+         }
+      },
+   });
+
+   // // Скрыть легенду
+   // twoAxisChart.options.legend.display = false;
+   // twoAxisChart.update();
+});
